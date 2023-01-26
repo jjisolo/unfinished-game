90d3448dc6b031b981a542982394d75a9114b1bf
daniel <jjisolo24@gmail.com>
2023-01-27 02:05:29 +0300

    Better logging

--- src/direct_engine/render_engine/DirectTextureFactory.cxx
+++ src/direct_engine/render_engine/DirectTextureFactory.cxx
@@ -2,20 +2,17 @@
 
 SDL_Texture* SDL::DirectTextureFactory::create_texture_from_image(SDL_Renderer* renderer_handle, const std::string& image_path)
 {
-    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Entering SDL::DirectTextureFactory::load_image [%p]", &SDL::DirectTextureFactory::create_texture_from_image);
-
     SDL_Texture* image_texture;
 
     auto load_direct_image = [this](SDL_Renderer* renderer_handle, const char* texture_path)
     {
-        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- Loading image %s", texture_path);
         SDL_Surface* image_surface;
         SDL_Texture* image_texture;
 
         image_surface = IMG_Load(texture_path);
         if(image_surface == NULL)
         {
-            SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- Image loading failed");
+            SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- Image %s loading failed", texture_path);
             SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- The following error message is: %s", IMG_GetError());
             throw SDL::DirectSystemException();
         }
@@ -23,12 +20,12 @@ SDL_Texture* SDL::DirectTextureFactory::create_texture_from_image(SDL_Renderer* 
         image_texture = SDL_CreateTextureFromSurface(renderer_handle, image_surface);
         if(image_texture == NULL)
         {
-            SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- Image convertation to texture failed");
+            SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- Image %s convertation to texture failed", texture_path);
             SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "\t--- The following error message is: %s", IMG_GetError());
             throw SDL::DirectSystemException();
         }
 
-        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- Cleaning up the image surface");
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- Loaded the texture %s", texture_path);
         SDL_FreeSurface(image_surface);
 
         return image_texture;
