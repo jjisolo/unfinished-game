a2c8d7ee6d4a2810860b1eb4b1c12a475f45d2df
daniel <jjisolo24@gmail.com>
2023-01-27 02:04:36 +0300

    Do not allow texture enable if it was enabled previously

--- src/direct_engine/render_engine/DirectTextureContainer.cxx
+++ src/direct_engine/render_engine/DirectTextureContainer.cxx
@@ -5,13 +5,21 @@
     // If the texture is enabled
     if(m_direct_object_state == SDL::DirectObjectState::Enabled)
     {
-        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t --- Rendering the texture %s", m_shared_texture_path.c_str());
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t --- Rendering the texture %s", m_direct_object_name.c_str());
         SDL_RenderCopy(renderer_handle, m_shared_texture.get(), &m_texture_source, &m_texture_destination);
     }
 }
 
-[[maybe_unused]] void SDL::DirectTextureContainer::enable(SDL::DirectRendererHandle renderer_handle)
+[[maybe_unused]] void SDL::DirectTextureContainer::enable(SDL::DirectRendererHandle renderer_handle, std::shared_ptr<SDL::DirectTextureFactory> direct_factory)
 {
+    // If the object is already enabled, we should either disable and then enable it,
+    // or do nothing
+    if(m_direct_object_state == SDL::DirectObjectState::Enabled)
+    {
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- The texture %s is already enabled", m_direct_object_name.c_str());
+        return;
+    }
+
     // We do not use logging here because we have full texture loading logging under the
     // DirectTextureFactory class
     switch(m_shared_texture_load_variant)
@@ -20,38 +28,51 @@
         case SDL::SharedTextureLoadVariant::LoadImageByPath:
         {
             // Load the texture from image, and wrap its pointer into the SharedDirectTexture(typedef) container
-            m_shared_texture = SDL::SharedDirectTexture(m_binded_texture_factory.create_texture_from_image(
+            //
+            // If we're unable to load the texture airbag, there's no texture going to be appearing on the screen,
+            // so we're making a maybe bad design choice to raise an error, and stop the program
+            m_shared_texture = SDL::SharedDirectTexture(direct_factory->create_texture_from_image(
                 renderer_handle,
-                m_shared_texture_path
+                m_direct_object_name
             ), SDL_DestroyTexture);
 
-            // Set the texture state to enabled(so it can be rendered)
-            m_direct_object_state = SDL::DirectObjectState::Enabled;
             break;
         }
         // Somehow the load variant is not specified, so we are loading the airbag. Just in case
         default:
         {
             // Load the airbag from image, and wrap its pointer into the SharedDirectTexture(typedef) container
-            m_shared_texture = SDL::SharedDirectTexture(m_binded_texture_factory.create_texture_from_image(
+            //
+            // Actually we're loading the airbag twice(if it does not exist), but who cares about the performance,
+            // when the internal structure of the render_object is broken
+            m_shared_texture = SDL::SharedDirectTexture(direct_factory->create_texture_from_image(
                     renderer_handle,
-                    m_binded_texture_factory.get_texture_airbag()
+                    direct_factory->get_texture_airbag()
             ), SDL_DestroyTexture);
 
-            // Set the texture state to enabled(so it can be rendered)
-            m_direct_object_state = SDL::DirectObjectState::Enabled;
             break;
         }
     }
+
+    // The code did not throw any exception at this time, so we are free
+    // to set the state to `Enabled` here, and avoid code repetition
+    //
+    // Set the texture state to enabled(so it can be rendered)
+    m_direct_object_state = SDL::DirectObjectState::Enabled;
 }
 
 [[maybe_unused]] void SDL::DirectTextureContainer::disable()
 {
-    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t --- Unloading texture %s", m_shared_texture_path.c_str());
+    if(m_direct_object_state == SDL::DirectObjectState::Disabled)
+    {
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- Texture %s is already disabled", m_direct_object_name.c_str());
+        return;
+    }
+
+    // Destroy the texture internal data
+    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "\t--- Unloading texture %s", m_direct_object_name.c_str());
+    SDL_DestroyTexture(m_shared_texture.get());
 
     // Disable the texture(do not render it)
     m_direct_object_state = SDL::DirectObjectState::Disabled;
-
-    // Destroy the texture internal data
-    SDL_DestroyTexture(m_shared_texture.get());
 }
