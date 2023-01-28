890e76134deca5f38871d971806a79afed5e1509
daniel <jjisolo24@gmail.com>
2023-01-28 19:06:20 +0300

    Create render-manager class

--- src/direct_engine/render_engine/RenderingManager.h
+++ src/direct_engine/render_engine/RenderingManager.h
@@ -1,12 +1,25 @@
 #pragma once
 
+#include <RenderingExceptions.h>
 #include <RenderingEngine.h>
+#include <RenderScene.h>
 
 namespace SDL
 {
+    using RenderSceneContainer = std::unordered_map<std::string, SDL::RenderScene>;
+    using RenderSceneTag       = std::string;
+
 	class RenderingManager
 	{
-	private:
-		int asd;
+    public:
+        [[maybe_unused]] void bind_render_scene      (const SDL::RenderSceneTag& render_scene_tag, SDL::RenderScene render_scene);
+
+        [[maybe_unused]] void set_active_render_scene(const SDL::RenderSceneTag& render_scene_tag);
+
+        [[maybe_unused]] void render                 ();
+
+    private:
+        SDL::RenderSceneContainer m_render_scene_container;
+        SDL::RenderSceneTag       m_active_render_scene;
 	};
 }
