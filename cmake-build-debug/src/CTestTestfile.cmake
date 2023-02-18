730052f90c27d35f6a5226bd7850759d755924b7
daniel <jjisolo24@gmail.com>
2023-02-18 20:59:56 +0300

    Project restructure

---
+++ cmake-build-debug/src/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/CLionProjects/DachshundAdventures/src
+# Build directory: /home/daniel/CLionProjects/DachshundAdventures/cmake-build-debug/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Backend")
+subdirs("Interface")
+subdirs("Rendering")
--- src/engine/render_engine/engine_interfaces/DirectObjectContainerPreset.cxx
+++
@@ -1,8 +0,0 @@
-//
-// Created by daniel on 1/26/23.
-//
-
-#include "DirectObjectContainerPreset.h"
-
-namespace SDL {
-} // SDL
